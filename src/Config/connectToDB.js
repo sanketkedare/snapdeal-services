aa839b7835f6dc5dd79ed2bdc584a5a173a21789
Sanket Kedare <sanketkedare200@gmail.com>
2024-06-26 15:58:32 +0530

    Bug Fixed

--- src/Config/connectToDB.js
+++ src/Config/connectToDB.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDB = async () => {
-  // const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/Snapdeal";
-  const DB_URL ="mongodb://localhost:27017/Snapdeal";
+  const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/Snapdeal";
 
   try 
   {
